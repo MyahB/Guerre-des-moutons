8e4a5d16e27be0083a78fe22aafcd56d4cca4015
Razo1erdunom <95338653+Razo1erdunom@users.noreply.github.com>
2022-02-04 09:44:37 +0300

    Update class Monde.py

--- class Monde.py
+++ class Monde.py
@@ -1,1 +1,12 @@
-class Monde(
+class Monde(dimension,duree_repousse,carte):
+  def init(self):
+    self.duree_repousse = duree_repousse
+    self.dimension = dimension
+    
+  def nbHerbe(self):
+    cpt = 0
+    for i in range(self.dimension):
+      for j in range(self.dimension):
+        if self.contenu[i][j]>self.duree_repousse:
+          cpt += 1
+    return cpt
